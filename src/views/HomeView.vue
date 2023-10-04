66f1e3ccdedd71de19c33be10c0f03081ff9c284
Amanda Ström <amandastromiths@hotmail.com>
2023-10-04 15:47:08 +0200

    added CommentComponent

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import SearchBoxComponent from '../components/SearchBoxComponent.vue';
+// import CommentComponent from '../components/CommentComponent.vue';
 </script>
 
 <template>
     <h1>HomeView</h1>
     <SearchBoxComponent></SearchBoxComponent>
+    <!-- <CommentComponent></CommentComponent> -->
 </template>
 <style scoped></style>
