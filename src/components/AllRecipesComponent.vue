f94c4c2fdba6f7c692f1bab96536bb14d207fac5
Amanda Ström <amandastromiths@hotmail.com>
2023-10-04 16:07:57 +0200

    added AllRecipesComponent

--- src/components/AllRecipesComponent.vue
+++ src/components/AllRecipesComponent.vue
@@ -1,1 +1,90 @@
 <!-- Task 2.6 Lista recept-->
+<script>
+export default { // Hämtar ut alla recept från API
+    data() {
+        return {
+            heading: "Alla recept",
+            recipes: [],
+            time: "min"
+        }
+    },
+    methods: {
+        async fetchData() {
+            this.recipes = null;
+            const response = await fetch("https://jau22-recept-grupp1-ijykxvjg4n3m.reky.se/recipes");
+            this.recipes = await response.json();
+        }
+    },
+    beforeMount() {
+        this.fetchData()
+    }
+}
+</script>
+
+<template>
+    <h1>{{ heading }}</h1>
+    <!-- Loopar genom och skriver ut enskilt recept på hemsida -->
+    <main v-for="recipe in recipes">
+        <!-- Layout med grid-container och grid-item för lite snyggare presentation(kan tas bort och ersättas med något annat) -->
+        <div class="grid-container">
+            <div class="grid-item recipe-head">
+                <RouterLink :to="`/recipe/${recipe._id}`">{{ recipe.title }} {{ recipe.ratings }}</RouterLink>
+            </div>
+            <div class="grid-item recipe-img"><img :src="recipe.imageUrl" alt="picture"></div>
+            <div class="grid-item recipe-main">{{ recipe.description }}</div>
+            <div class="grid-item recipe-foot">{{ recipe.timeInMins }} {{ time }}</div>
+        </div>
+    </main>
+</template>
+
+<style>
+div {
+    color: black;
+    background-color: whitesmoke;
+}
+
+h1 {
+    text-align: center;
+}
+
+img {
+    height: 100px;
+    width: 100px;
+    float: inline-start;
+
+}
+
+.recipe-head {
+    grid-area: head;
+    font-size: 30px;
+    text-align: center;
+}
+
+.recipe-img {
+    grid-area: img;
+}
+
+.recipe-main {
+    grid-area: main;
+}
+
+.recipe-foot {
+    grid-area: foot;
+}
+
+.grid-item {
+    background-color: darkgrey;
+}
+
+.grid-container {
+    display: grid;
+    grid-template-areas:
+        'head head head head head head'
+        'main main main main img img'
+        'foot foot foot foot img img';
+    margin-left: 10%;
+    margin-right: 10%;
+    margin-bottom: 5px;
+    margin-top: 5px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import SearchBoxComponent from '../components/SearchBoxComponent.vue';
+import AllRecipesComponent from '../components/AllRecipesComponent.vue'
 // import CommentComponent from '../components/CommentComponent.vue';
 </script>
 
 <template>
     <h1>HomeView</h1>
     <SearchBoxComponent></SearchBoxComponent>
+    <AllRecipesComponent></AllRecipesComponent>
     <!-- <CommentComponent></CommentComponent> -->
 </template>
 <style scoped></style>
