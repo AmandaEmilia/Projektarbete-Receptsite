47b6b4ba3518a148f7d496f4e80708a71ede3fbb
Wijdan Cederlid <wijdan.cederlid@iths.se>
2023-10-04 20:44:36 +0200

    Added Comment component

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -1,7 +1,19 @@
 <script>
+import CommentComponent from '../components/CommentComponent.vue';
+export default {
+    components: {
+    CommentComponent
+},
+    data(){
+      return{
+        
+      }
+    }
+  }
 </script>
 
 <template>
     <h1>RecipeView</h1>
+    <CommentComponent></CommentComponent>
 </template>
 <style scoped></style>
