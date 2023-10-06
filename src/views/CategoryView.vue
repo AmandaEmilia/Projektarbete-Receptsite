24f5ee4fa59d113866a9c9699520ea21337cc80e
Amanda Ström <amandastromiths@hotmail.com>
2023-10-06 12:50:50 +0200

    adding changes

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -35,19 +35,15 @@ export default {
 <template>
     <CategoryListComponent></CategoryListComponent>
     <h1>{{ this.$route.params.categoryId }}</h1>
-
     <div v-for="recipe in category">
-
         <div class="grid-container">
             <div class="grid-item recipe-head">
-                <RouterLink :to="`/recipe/${recipe.id}`">{{ recipe.title }} {{ recipe.ratings }}</RouterLink>
+                <RouterLink :to="`/recipe/${recipe.id}`">{{ recipe.title }} {{ recipe.avgRating }}</RouterLink>
             </div>
             <div class="grid-item recipe-img"><img :src="recipe.imageUrl" alt="picture"></div>
             <div class="grid-item recipe-main">{{ recipe.description }}</div>
             <div class="grid-item recipe-foot">{{ recipe.timeInMins }} {{ time }}</div>
         </div>
-
-
     </div>
 </template>
 <style scoped></style>
