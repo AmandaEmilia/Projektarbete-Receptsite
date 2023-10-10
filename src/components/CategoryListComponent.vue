eebe4935c0165289686c15662e5c40439b705985
Amanda Ström <amandastromiths@hotmail.com>
2023-10-10 21:13:53 +0200

    adding :key in v-for in categoryList

--- src/components/CategoryListComponent.vue
+++ src/components/CategoryListComponent.vue
@@ -28,7 +28,7 @@ export default {
 <template>
     <h1>{{ heading }}</h1>
 
-    <nav v-for="category in categories">
+    <nav v-for="category in categories" :key="categories._id">
         <RouterLink :to="`/category/${category.name}`">{{ category.name }} ({{ category.count }})</RouterLink>
     </nav>
 </template>
