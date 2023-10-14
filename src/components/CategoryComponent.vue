1d616e8f981c281b5704578a3561fef2a51e06e3
Amanda Ström <amandastromiths@hotmail.com>
2023-10-14 14:56:14 +0200

    refactoring from Cat.View to own Cat.Comp

---
+++ src/components/CategoryComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <h1>{{ this.$route.params.categoryId }}</h1>
+    <div v-for="recipe in category" :key="recipe._id">
+        <div class="grid-container">
+            <div class="grid-item recipe-head">
+                <RouterLink :to="`/recipes/${recipe._id}`">{{ recipe.title }}</RouterLink>
+            </div>
+            <div class="grid-item recipe-img"><img :src="recipe.imageUrl" alt="picture"></div>
+            <div class="grid-item recipe-main">{{ recipe.description }}</div>
+            <div class="grid-item recipe-foot">{{ recipe.timeInMins }} {{ time }}</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            category: "",
+            categoryId: ""
+        }
+    },
+    methods: {
+        async fetchData() {
+            this.category = null;
+            const response = await fetch(`https://jau22-recept-grupp1-ijykxvjg4n3m.reky.se/categories/${this.$route.params.categoryId}/recipes`)
+            this.category = await response.json();
+        }
+    },
+    created() {
+        this.$watch(
+            () => this.$route.params,
+            (toParams, previousParams) => {
+                this.categoryId = this.fetchData(toParams || "");
+            }
+        )
+    },
+    mounted() {
+        this.fetchData();
+    }
+}
+</script>
--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -2,49 +2,17 @@
 <template>
     <CategorySearchBox></CategorySearchBox>
     <CategoryListComponent></CategoryListComponent>
-    <h1>{{ this.$route.params.categoryId }}</h1>
-    <div v-for="recipe in category">
-        <div class="grid-container">
-            <div class="grid-item recipe-head">
-                <RouterLink :to="`/recipes/${recipe._id}`">{{ recipe.title }}</RouterLink>
-            </div>
-            <div class="grid-item recipe-img"><img :src="recipe.imageUrl" alt="picture"></div>
-            <div class="grid-item recipe-main">{{ recipe.description }}</div>
-            <div class="grid-item recipe-foot">{{ recipe.timeInMins }} {{ time }}</div>
-        </div>
-    </div>
+    <CategoryComponent></CategoryComponent>
 </template>
 <script>
 import CategoryListComponent from '../components/CategoryListComponent.vue'
 import CategorySearchBox from '../components/CategorySearchBox.vue';
+import CategoryComponent from '../components/CategoryComponent.vue'
 export default {
     components: {
         CategoryListComponent,
-        CategorySearchBox
-    },
-    data() {
-        return {
-            category: "",
-            categoryId: ""
-        }
-    },
-    methods: {
-        async fetchData() {
-            this.category = null;
-            const response = await fetch(`https://jau22-recept-grupp1-ijykxvjg4n3m.reky.se/categories/${this.$route.params.categoryId}/recipes`)
-            this.category = await response.json();
-        }
-    },
-    created() {
-        this.$watch(
-            () => this.$route.params,
-            (toParams, previousParams) => {
-                this.categoryId = this.fetchData(toParams || "");
-            }
-        )
-    },
-    mounted() {
-        this.fetchData();
+        CategorySearchBox,
+        CategoryComponent
     }
 }
 </script>
