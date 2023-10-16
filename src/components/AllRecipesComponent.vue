4e2ee84b60ac4d4276d3f1743bc9aef15db4e9c3
Amanda Ström <amandastromiths@hotmail.com>
2023-10-16 17:19:32 +0200

    removed one starComp.

--- src/components/AllRecipesComponent.vue
+++ src/components/AllRecipesComponent.vue
@@ -16,7 +16,6 @@
                 <div class="grid-item recipe-img"><img :src="recipe.imageUrl" alt="picture"></div>
                 <div class="grid-item recipe-description">{{ recipe.description }}</div>
                 <div class="grid-item recipe-foot">
-                    <StarComponent :avg-rating="`${recipe.avgRating}`"></StarComponent> |
                     {{ recipe.ingredients.length }} {{ nrOfIngredients }} |
                     {{ recipe.timeInMins }} {{ time }}
                 </div>
